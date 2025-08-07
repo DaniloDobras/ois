3a3b474321d870a91575b3f14667efd4ae862470
Zeljko Bozic <zeljko.zbozic@gmail.com>
2025-08-07 22:34:58 +0200

    Removed Int db and allowed requests from Angular app.

--- app/main.py
+++ app/main.py
@@ -5,13 +5,26 @@ from fastapi import FastAPI
 from app.api.routes import router
 from app.db.database import init_db
 
+from fastapi.middleware.cors import CORSMiddleware
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    init_db()
-    yield
-app = FastAPI(lifespan=lifespan)
+
+app = FastAPI()
+
+# Allow requests from Angular app
+origins = [
+    "http://localhost:4200",  # Angular dev server
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # origins allowed to access
+    allow_credentials=True,
+    allow_methods=["*"],              # allow all HTTP methods
+    allow_headers=["*"],              # allow all headers
+)
+
 app.include_router(router)
+
 
 @app.get("/")
 async def root():
